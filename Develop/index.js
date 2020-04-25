7021f1b5752276b6fabcd4df4c0789c4fd16e73e
hafiz salman <hmsalmans1@gmail.com>
2020-04-25 21:54:34 +0500

    amending index file

--- Develop/index.js
+++ Develop/index.js
@@ -1,33 +1,88 @@
-var inquirer = require("inquirer");
+ const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
+const axios = require('axios');
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your user name?",
-      name: "username"
-    },
-    {
-      type: "password",
-      message: "What is your password?",
-      name: "password"
-    },
-    {
-      type: "password",
-      message: "Re-enter password to confirm:",
-      name: "confirm"
-    }
-  ])
-  .then(function(response) {
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your Github username",
+            name: "github_user"
+        },
+        {
+            type: "input",
+            message: "What is your Github repository",
+            name: "github_repo"
+        },
+        {
+            type: "input",
+            message: "What is your project title?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "What is your project version",
+            name: "version",
+        },
+        {
+            type: "input",
+            message: "What is your project description",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "What is your project usage",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "What is your project license",
+            name: "license"
+        },
+        {
+            type: "input",
+            message: "What is the project license url",
+            name: "license_url"
+        },
+        {
+            type: "input",
+            message: "Who are your project contributors",
+            name: "contributors"
+        },
+        {
+            type: "input",
+            message: "What is your project test command",
+            name: "test"
+        },
+        {
+            type: "input",
+            message: "What email can users reach you at if they have questions?",
+            name: "questions"
+        },
+        
+      ]);
+};
 
-    if (response.confirm === response.password) {
-      console.log("Success!");
-    }
-    else {
-      console.log("You forgot your password already?!");
-    }
-  });
+promptUser().then(async function(response) {
+    console.log(response);
 
+    const url = `https://api.github.com/users/${response.github_user}`;
+
+    const data = await axios.get(url);
+    // console.log(data)
+    
+    
+    const markdown = generateMarkdown(response);
+
+    fs.writeFile('testREADME.md', markdown, function(error) {
+        if (error) {
+            return console.log(error);
+        } else {
+            console.log('success')
+        }
+    });
+});
 
 
 
@@ -40,7 +95,7 @@ const questions = [
 
 ];
 
-function writeToFile(fileName, data) {
+function writeToFile("Readme.md", data) {
 }
 
 function init() {
